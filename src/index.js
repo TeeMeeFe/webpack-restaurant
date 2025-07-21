eaeb04bee3ec3b51359eb46a64f0160e5e00c454
TeeMeeFe <39315813+TeeMeeFe@users.noreply.github.com>
2025-07-20 21:03:02 -0300

    Add a screenController function with an event handler to handle page tabbing

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import { buildAboutPage } from "./pages/about";
 import { buildHomePage } from "./pages/home";
 import { buildMenuPage } from "./pages/menu";
 
-buildAboutPage();
-buildHomePage();
-buildMenuPage();
+const screenController = () => {
+    const nav = document.querySelector("nav"); // Prolly not a good idea to go with this low level of specificity but whatever
 
-console.log("Script loaded successfully...");
+    nav.addEventListener("click", e => {
+        const button = e.target.tagName === "BUTTON";
+        // Escape early if it isn't a button
+        if(!button) return;
+
+        switch(e.target.classList.value) {
+            case "home":
+                buildAboutPage();
+                break
+            case "menu":
+                buildHomePage();
+                break
+            case "about":
+                buildMenuPage();
+                break
+        }
+    });
+    
+    console.log("Script loaded successfully...");
+}
+
+screenController();
